2d6cc52786d9e63e710e142e3f93782300f388dd
josef-karpinski <121736137+josef-karpinski@users.noreply.github.com>
2023-07-03 10:50:32 -0400

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -11,8 +11,19 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+    //Write the authenication mechanism here
+        if(req.session.authrization){
+        const token = req.session.authrization['accesstoken'];
+        jwt.verify(token, 'access', (err, user) => {
+            if(!err) {
+                req.user = user;
+                next();
+            } else {
+                return res.status(403).json({message: "Invalid token"});
+            }
+        });
+    }
+    });
  
 const PORT =5000;
 
